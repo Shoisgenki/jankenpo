c5f288e0894900a4169b427f23cad97db32becb4
Shoyo <shoyoisgenki@gmail.com>
2024-04-15 01:01:36 +0530

    Implemented playRound function for a single round of Rock Paper Scissors. Made playerSelection case-insensitive for user input variations

--- saishogun.js
+++ saishogun.js
@@ -1,4 +1,3 @@
-
 function getRandIndex(){
     return Math.floor(Math.random() * 3);
 }
@@ -8,5 +7,39 @@ function getComputerChoice(choice1, choice2, choice3){
     return choices[getRandIndex()];
 }
 
-console.log(getComputerChoice("Rock", "Paper", "Scissors"));
+function playRound(){
+    var computerSelection = getComputerChoice("Rock", "Paper", "Scissors").toLowerCase();
+
+    var playerSelection = prompt("Enter your choice").toLowerCase();
+
+    if (computerSelection == playerSelection) {
+        console.log("It's a tie!");
+    }
+
+    else if (computerSelection == "rock" & playerSelection == "paper") {
+        console.log("You win! Paper beats Rock.");
+    }
+
+    else if (computerSelection == "paper" & playerSelection == "rock") {
+        console.log("You lose! Paper beats Rock.");
+    }
+
+    else if (computerSelection == "scissors" & playerSelection == "paper") {
+        console.log("You lose! Scissors beat Paper.");
+    }
+
+    else if (computerSelection == "scissors" & playerSelection == "rock") {
+        console.log("You win! Rock beats Scissors.");
+    }
+
+    else if (computerSelection == "paper" & playerSelection == "scissors") {
+        console.log("You win! Scissors beat Paper.");
+    }
+
+    else {
+        console.log("You lose! Rock beats Scissors")
+    }
+}
+
+playRound();
 
