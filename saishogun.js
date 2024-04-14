4b2cde8cc78d9903b58cf895f7d8e9fa3b2a878e
Shoyo <shoyoisgenki@gmail.com>
2024-04-14 21:49:42 +0530

    Added a function that'll return Rock, Paper, Scissors randomly

--- saishogun.js
+++ saishogun.js
@@ -1,1 +1,12 @@
-console.log("Hello World!")
+
+function getRandIndex(){
+    return Math.floor(Math.random() * 3);
+}
+
+function getComputerChoice(choice1, choice2, choice3){
+    var choices = [choice1, choice2, choice3];
+    return choices[getRandIndex()];
+}
+
+console.log(getComputerChoice("Rock", "Paper", "Scissors"));
+
