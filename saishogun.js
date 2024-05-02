2cbd720c1858a2cdf0ebc3c8df08f5130f96eb61
Shoyo <shoyoisgenki@gmail.com>
2024-05-02 12:15:58 +0530

    Completed my jankenpo! :)

--- saishogun.js
+++ saishogun.js
@@ -1,57 +1,36 @@
+document.getElementById("rock-button").addEventListener("click", function() {
+    playRound("rock");
+});
+
+document.getElementById("paper-button").addEventListener("click", function() {
+    playRound("paper");
+});
+
+document.getElementById("scissors-button").addEventListener("click", function() {
+    playRound("scissors");
+});
+
+res = document.getElementById("result");
+
 function getComputerChoice(){
     function getRandIndex(){
         return Math.floor(Math.random() * 3);
     }
-    var choices = ["Rock", "Paper", "Scissors"];
+    var choices = ["rock", "paper", "scissors"];
     return choices[getRandIndex()];
 }
 
-function playRound(){
-    var computerSelection = getComputerChoice().toLowerCase();
+function playRound(playerSelection){
+    var computerSelection = getComputerChoice();
 
-    var playerSelection = prompt("Enter your choice").toLowerCase();
-
-    if (computerSelection == playerSelection) {
-        return ("It's a tie!");
-    }
-
-    else if (computerSelection == "rock" & playerSelection == "paper" || computerSelection == "paper" & playerSelection == "scissors" || computerSelection == "scissors" & playerSelection == "rock") {
-        return("You win!" + " " + playerSelection + " " + "beats" + " " + computerSelection);
-    }
-
-    else {
-        return ("You lose! " + computerSelection + " beats " + playerSelection);
+    if (computerSelection === playerSelection) {
+        res.textContent = "It's a Tie!";
+    } else if ((computerSelection === "rock" && playerSelection === "paper") ||
+               (computerSelection === "paper" && playerSelection === "scissors") ||
+               (computerSelection === "scissors" && playerSelection === "rock")) {
+        res.textContent = "You win! " + playerSelection.toUpperCase() + " beats " + computerSelection.toUpperCase();
+    } else {
+        res.textContent = "You lose! " + computerSelection.toUpperCase() + " beats " + playerSelection.toUpperCase();
     }
 }
 
-function playGame(){
-    let playerScore = 0;
-    let computerScore = 0;
-    let tiedRounds = 0;
-    for(i=0; i<5; i++){
-        let result = playRound();
-        if (result.includes('win')) {
-            playerScore++;
-        }
-        else if (result.includes('lose')) {
-            computerScore++;
-        }
-        else {
-            tiedRounds++;
-        }
-    }
-
-    if (playerScore > computerScore){
-        console.log("You win! with a score of " + playerScore + " - " + computerScore);
-    }
-    else if (computerScore > playerScore){
-        console.log("You lose! with a score of " + playerScore + " - " + computerScore);
-    }
-    else {
-        console.log("Tie!");
-    }
-
-    console.log("Total tied rounds: " + tiedRounds);
-}
-
-playGame();
